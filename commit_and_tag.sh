2219cfa23b5b46e6e1432a44a6bd93f96c79ea9c
Ali Samji <ali@ybor.ai>
2025-10-02 09:38:47 -0500

    ci: Remove dummy values from script.

--- commit_and_tag.sh
+++ commit_and_tag.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 set -euo pipefail
-GITHUB_REPOSITORY=alisamji-ybor/dummy
-GITHUB_REF_NAME=main
 SHA=$(gh api '/repos/'$GITHUB_REPOSITORY'/branches/'$GITHUB_REF_NAME | jq -er .commit.sha)
-RUNNER_TEMP='.'
-GITHUB_ACTION_PATH='.'
 cp $GITHUB_ACTION_PATH/createCommit.json $RUNNER_TEMP/body.json
 
 BODY=$(cat $RUNNER_TEMP/body.json |\
@@ -15,13 +11,14 @@ BODY=$(cat $RUNNER_TEMP/body.json |\
     yq -o json -I0)
 echo "$BODY" > $RUNNER_TEMP/body.json
 
-CHANGED_FILES=$(cat) # Read JSON array from stdin
+CHANGED_FILES=$(cat) # Read space-separated paths of files.
 cat $RUNNER_TEMP/body.json
 for file in $CHANGED_FILES; do
     yq -io json -I0 '.variables.input.fileChanges.additions += {"path": "'$file'", "contents": "'$(base64 -w0 -i $file)'"}' $RUNNER_TEMP/body.json
 done
 
-yq $RUNNER_TEMP/body.json
+echo 'Create Commit Request Body:'
+yq -o json $RUNNER_TEMP/body.json
 
 # Using the gh cli produces a VERIFIED commit.
 RESPONSE=$(gh api graphql --input body.json)
