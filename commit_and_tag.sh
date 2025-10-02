9a7fdb104f48cb628e8600a17c67a85ba27c9941
Ali Samji <ali@ybor.ai>
2025-10-02 16:03:31 -0500

    fix: Remove hardcoded dummy repo name.

--- commit_and_tag.sh
+++ commit_and_tag.sh
@@ -28,7 +28,7 @@ echo "$RESPONSE" | yq -o json
 NEW_SHA=$(echo "$RESPONSE" | jq -er '.data.createCommitOnBranch.commit.oid')
 VERSION=$1
 HELM_VERSION=${2:-}
-gh api -X POST /repos/p6m-dev/token-exchange-demo/git/refs -f "ref=refs/tags/v$VERSION" -f "sha=$NEW_SHA"
+gh api -X POST /repos/$GITHUB_REPOSITORY/git/refs -f "ref=refs/tags/v$VERSION" -f "sha=$NEW_SHA"
 if [ -n "$HELM_VERSION" ]; then
-  gh api -X POST /repos/p6m-dev/token-exchange-demo/git/refs -f "ref=refs/tags/helm-v$HELM_VERSION" -f "sha=$NEW_SHA"
+  gh api -X POST /repos/$GITHUB_REPOSITORY/git/refs -f "ref=refs/tags/helm-v$HELM_VERSION" -f "sha=$NEW_SHA"
 fi
