61fd4de24416b948b7875685417a81281211bef1
Ali Samji <ali@ybor.ai>
2025-10-02 11:01:23 -0500

    fix: file path for gh api command.

--- commit_and_tag.sh
+++ commit_and_tag.sh
@@ -6,7 +6,7 @@ cp $GITHUB_ACTION_PATH/createCommit.json $RUNNER_TEMP/body.json
 BODY=$(cat $RUNNER_TEMP/body.json |\
     yq '.variables.input.branch.branchName = "'$GITHUB_REF_NAME'"' |\
     yq '.variables.input.branch.repositoryNameWithOwner = "'$GITHUB_REPOSITORY'"' |\
-    yq '.variables.input.message.headline = "chore: Sync Helm Chart appVersion."' |\
+    yq '.variables.input.message.headline = "chore: Bump Versions."' |\
     yq '.variables.input.expectedHeadOid = "'$SHA'"' |\
     yq -o json -I0)
 echo "$BODY" > $RUNNER_TEMP/body.json
@@ -21,7 +21,7 @@ echo 'Create Commit Request Body:'
 yq -o json $RUNNER_TEMP/body.json
 
 # Using the gh cli produces a VERIFIED commit.
-RESPONSE=$(gh api graphql --input body.json)
+RESPONSE=$(gh api graphql --input $RUNNER_TEMP/body.json)
 echo "$RESPONSE" | yq -o json
 
 # TODO: Use response to create tag and reference for the returned oid.
